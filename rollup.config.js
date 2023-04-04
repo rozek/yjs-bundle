d0e71cbb2fda0c0ab38de26cc482b047714d5fe1
Andreas Rozek <A.Rozek@gmx.de>
2023-04-04 13:22:57 +0200

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+// see https://github.com/rozek/build-configuration-study
+
+import commonjs   from '@rollup/plugin-commonjs'
+import resolve    from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+  input:    './src/yjs-bundle.ts',
+  output: [
+    { 
+      file:      './dist/yjs-bundle.js',
+      format:    'umd', // builds for both Node.js and Browser
+      name:      'yjs', // required for UMD modules
+      noConflict:true,
+      sourcemap: true,
+    },{
+      file:      './dist/yjs-bundle.esm.js',
+      format:    'esm',
+      sourcemap: true,
+    }
+  ],
+  plugins: [
+    resolve(), commonjs(), typescript(),
+    terser({ format:{ comments:false, safari10:true } })
+  ],
+};
