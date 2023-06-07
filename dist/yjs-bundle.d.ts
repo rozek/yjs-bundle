cabe1ce17986d5437028d6e7e02d350e99d7dce3
Andreas Rozek <A.Rozek@gmx.de>
2023-06-07 15:23:07 +0200

    new build

--- dist/yjs-bundle.d.ts
+++ dist/yjs-bundle.d.ts
@@ -3,4 +3,5 @@ import { PREFERRED_TRIM_SIZE, fetchUpdates, storeState, clearDocument, Indexeddb
 import { WebsocketProvider, messageAuth, messageAwareness, messageQueryAwareness, messageSync } from 'y-websocket';
 import { WebrtcConn, Room, SignalingConn, WebrtcProvider } from 'y-webrtc';
 import { YKeyValue } from 'y-utility/y-keyvalue';
-export { PREFERRED_TRIM_SIZE, fetchUpdates, storeState, clearDocument, IndexeddbPersistence, WebsocketProvider, messageAuth, messageAwareness, messageQueryAwareness, messageSync, WebrtcConn, Room, SignalingConn, WebrtcProvider, YKeyValue };
+import { LWWMap } from 'y-lwwmap';
+export { PREFERRED_TRIM_SIZE, fetchUpdates, storeState, clearDocument, IndexeddbPersistence, WebsocketProvider, messageAuth, messageAwareness, messageQueryAwareness, messageSync, WebrtcConn, Room, SignalingConn, WebrtcProvider, YKeyValue, LWWMap };
